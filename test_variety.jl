5cb50dddd1d1e7715e204e996ac65782151b9ee3
Jay Patel <jaypat10.JP@hotmail.com>
2025-01-15 16:25:25 +0000

    Updating files

---
+++ test_variety.jl
@@ -0,0 +1,15 @@
+using Oscar
+
+# This is how to compute 0-dimensional Tropical Varieties
+# There are faster ways, not official yet
+
+R, (x,y) = polynomial_ring(QQ, ["x", "y"])
+
+nu = tropical_semiring_map(QQ, 3) # p-adic valuation
+
+f = x - 2
+g = y - 2
+
+I = ideal([f,g])
+
+Vector{QQFieldElem}.(vertices(first(tropical_variety(I, nu)))) 
