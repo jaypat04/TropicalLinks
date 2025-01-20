b6b556afe78e423b9924c93f16386aa56802ff18
Jay Patel <jaypat10.JP@hotmail.com>
2025-01-20 17:50:50 +0000

    Added plot_variety.jl Updated functions.jl, test.jl

--- functions.jl
+++ functions.jl
@@ -2,7 +2,31 @@ function foo()
     println("Hello, World!")
 end
 
+
+
+#Assuming d = 1
 function tropical_link(I, nu)
-    
+    R = base_ring(I)
+    n = ngens(R) # Number of variables
+
+    for i in 1:n
+        p = base_ring(R)(uniformizer(nu)) # Uniformizer
+
+        Risymbols = copy(symbols(R)) # Copy the list of symbols
+        deleteat!(Risymbols, i) # Remove the i-th symbol
+
+        Ri, xWithouti = polynomial_ring(QQ, Risymbols) # Define the polynomial ring without the i-th variable
+
+        phiplus = hom(R, Ri, insert!(copy(xWithouti), i, Ri(p))) # Define the homomorphism for the positive map
+
+        phiminus = hom(R, Ri, insert!(copy(xWithouti), i, Ri(p^(-1)))) # Define the homomorphism for the negative map
+
+        Jplus = phiplus(I) # Apply the positive map
+        Jminus = phiminus(I) # Apply the negative map
+
+        Tplus = Vector{QQFieldElem}.(vertices(first(tropical_variety(Jplus, nu)))) # Compute the tropical variety for the positive map
+        Tminus = Vector{QQFieldElem}.(vertices(first(tropical_variety(Jminus, nu)))) # Compute the tropical variety for the negative map
+        
+    end
 
 end
--- test.jl
+++ test.jl
@@ -9,3 +9,22 @@ I = ideal([f]) # Input
 nu = tropical_semiring_map(QQ, 3) # p-adic valuation
 
 tropical_link(I, nu)
+
+i = 2
+p = base_ring(R)(uniformizer(nu))
+
+Risymbols = copy(symbols(R)) # Copy the list of symbols
+deleteat!(Risymbols, i) # Remove the i-th symbol
+
+Ri, xWithouti = polynomial_ring(QQ, Risymbols)
+
+phiplus = hom(R, Ri, insert!(copy(xWithouti), i, Ri(p)))
+
+phiminus = hom(R, Ri, insert!(copy(xWithouti), i, Ri(p^(-1))))
+
+Jplus = phiplus(I)
+Jminus = phiminus(I)
+
+Tplus = Vector{QQFieldElem}.(vertices(first(tropical_variety(Jplus, nu)))) 
+Tminus = Vector{QQFieldElem}.(vertices(first(tropical_variety(Jminus, nu))) )
+
